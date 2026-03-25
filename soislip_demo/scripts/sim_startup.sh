7144c6b6643ad6dca1d907bc81fc6cba09be8239
dudajulian <dudajulian@proton.me>
2026-03-25 16:50:06 +0000

    zed camera jetson

--- soislip_demo/scripts/sim_startup.sh
+++ soislip_demo/scripts/sim_startup.sh
@@ -22,33 +22,43 @@ ELEVATION_CONFIG_DIR="$DEMO_SHARE/config/elevation_mapping"
 RVIZ_CONFIG_FILE="$DEMO_SHARE/config/rviz/custom_rviz2.rviz"
 
 declare -A cmds=(
+  [coppelia_sim]="'$COPPELIASIM_ROOT_DIR'/coppeliaSim \
+    '$ROBOT_RESOURCE_DIR'/scene.ttt -s 0 "
+#     > '$LOG_DIR/coppelia_sim.log' 2>&1"
+  [coppelia_controller]="ros2 launch coppelia_ros2_control coppelia_control.launch.py \
+    controller_name:=platform_velocity_controller \
+    controller_config_path:='$ROBOT_RESOURCE_DIR/control.yaml' \
+    robot_description_path:='$ROBOT_RESOURCE_DIR/robot.urdf' "
+#     > '$LOG_DIR/coppelia_controller.log' 2>&1"
   [elevation_mapping]="ros2 run elevation_mapping elevation_mapping \
     --ros-args \
-    --params-file '$ELEVATION_CONFIG_DIR/zed2_robot.yaml' \
+    --params-file '$ELEVATION_CONFIG_DIR/sim_robot.yaml' \
     --params-file '$ELEVATION_CONFIG_DIR/elevation_map.yaml' \
     --params-file '$ELEVATION_CONFIG_DIR/aslam.yaml' \
-    --params-file '$ELEVATION_CONFIG_DIR/postprocessor_pipeline.yaml'"
+    --params-file '$ELEVATION_CONFIG_DIR/postprocessor_pipeline.yaml' "
 #     > '$LOG_DIR/elevation_mapping.log' 2>&1"
   [soislip_demo]="ros2 launch soislip_core soislip.launch.py \
-    params_file:='$DEMO_SHARE/config/params.yaml'"
+    params_file:='$DEMO_SHARE/config/sim_params.yaml' "
 #     > '$LOG_DIR/soislip_demo.log' 2>&1"
   [rviz]="ros2 run rviz2 rviz2 \
     --display-config '$RVIZ_CONFIG_FILE'"
   [teleop_key]="ros2 run teleop_twist_keyboard teleop_twist_keyboard \
-    --ros-args -p stamped:=true \
+    --ros-args -p stamped:=true -p use_sim_time:=true \
     --remap cmd_vel:=/platform_velocity_controller/cmd_vel" 
   [teleop_joy]="ros2 launch teleop_twist_joy teleop-launch.py \
-    joy_config:='xbox' publish_stamped_twist:=true  \
+    joy_config:='xbox' publish_stamped_twist:=true use_sim_time:=true \
     joy_vel:=/platform_velocity_controller/cmd_vel" 
 )
 
 # Keep startup order aligned with all.launch.py.
 startup_order=(
+  coppelia_sim
+  coppelia_controller
   elevation_mapping
-  # soislip_demo
+  soislip_demo
   rviz
 #   teleop_key
-  # teleop_joy
+  teleop_joy
 )
 
 if tmux has-session -t "$SESSION" 2>/dev/null; then
