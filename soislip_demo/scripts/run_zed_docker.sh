3b262041b4d79f800c11de957506d31cf583b752
dudajulian <dudajulian@proton.me>
2026-04-02 12:50:20 +0000

    update startup scripts

---
+++ soislip_demo/scripts/run_zed_docker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Determine container name (does not support suffixes)
+PLATFORM="$(uname -m)"
+CONTAINER_NAME="isaac_ros_dev-${PLATFORM}-container"
+
+# Start the exited container and run the script.
+if [ "$(docker ps -a --quiet --filter status=exited --filter name=${CONTAINER_NAME})" ]; then
+    docker start ${CONTAINER_NAME}
+    ISAAC_ROS_WS=$(docker exec ${CONTAINER_NAME} printenv ISAAC_ROS_WS)
+    echo "Docker workspace: ${ISAAC_ROS_WS}"
+
+    docker exec -i -t -u admin --workdir ${ISAAC_ROS_WS} ${CONTAINER_NAME} /bin/bash -lc "source ~/.bashrc && ./start_zed_node.sh"
+    exit 0
+fi
+
+# Attach to the container, or start it if it doesn't exist.
+cd ${ISAAC_ROS_WS}/src/isaac_ros_common && \
+./scripts/run_dev.sh -i ros2_humble.zed \
+-a "-v /usr/local/zed/settings:/usr/local/zed/settings \
+    -v /usr/local/zed/resources:/usr/local/zed/resources" \
+--  -lc './source_zed.sh && ./start_zed_node.sh; exec bash'
--- soislip_demo/scripts/startup.sh
+++ soislip_demo/scripts/startup.sh
@@ -22,7 +22,8 @@ ELEVATION_CONFIG_DIR="$DEMO_SHARE/config/elevation_mapping"
 RVIZ_CONFIG_FILE="$DEMO_SHARE/config/rviz/custom_rviz2.rviz"
 
 declare -A cmds=(
-  [zenoh_bridge]="zenoh-bridge-ros2dds -e tcp/192.168.131.1:7447"
+  [zenoh_bridge]="zenoh-bridge-ros2dds -c $DEMO_PKG_DIR/resources/zenoh_config.json5"
+  [zed_docker]="./$DEMO_PKG_DIR/scripts/run_zed_docker.sh"
   [static_tf_zed]="ros2 run tf2_ros static_transform_publisher \
     0.05 0.0 0.3 0 0.174533 0 base_link zed_camera_link \
     --ros-args \
@@ -52,11 +53,12 @@ declare -A cmds=(
 startup_order=(
   zenoh_bridge
   static_tf_zed
+  zed_docker
   # elevation_mapping
   # soislip_demo
   # rviz
   # teleop_key
-  teleop_joy
+  # teleop_joy
 )
 
 if tmux has-session -t "$SESSION" 2>/dev/null; then
